2c9393ea9a8d5be11682c0da8d2a0a913cd16089
prince <yichunlu707@gmail.com>
2026-03-20 14:43:59 +0800

    Initial commit

---
+++ GTZAN/train.sh
@@ -0,0 +1,16 @@
+pip install -r requirements.txt
+python train.py \
+  --csv_path /Users/princelu/Desktop/NN/GTZAN/gtzan.csv \
+  --audio_root /Users/princelu/Desktop/NN/GTZAN/genres \
+  --out_dir /Users/princelu/Desktop/NN/GTZAN/checkpoints \
+  --hidden_dim 256 \
+  --num_layers 2 \
+  --dropout 0.1 \
+  --batch_size 64 \
+  --epochs 100 \
+  --validate_every 10 \
+  --lr 1e-3 \
+  --weight_decay 1e-4 \
+  --timeseries_length 128 \
+  --hop_length 512 \
+  --target_sr 22050
